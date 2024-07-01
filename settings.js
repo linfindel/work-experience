3039d6c8d7ee67c7feb622b0b552eddbe45ee4c3
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-07-01 16:20:36 +0100

    Add release notes system

--- settings.js
+++ settings.js
@@ -11,3 +11,21 @@ function setSport(sport) {
 }
 
 document.getElementById(localStorage.getItem("sport")).style.backgroundColor = "rgba(0, 89, 255, 0.5)";
+
+const username = 'linfindel';
+const repo = 'work-experience';
+
+fetch(`https://api.github.com/repos/${username}/${repo}/commits?per_page=1`)
+  .then(response => {
+    const totalCount = response.headers.get('Link').match(/page=(\d+)>; rel="last"/)[1] / 100;
+    return response.json().then(data => {
+      const latestCommitMessage = data[0].commit.message;
+
+      document.getElementById("version").innerText = `Version ${totalCount.toFixed(2)} Release Notes`;
+      document.getElementById("whats-new").innerText = latestCommitMessage;
+
+      document.getElementById("about-card").style.opacity = "1";
+      document.getElementById("about-card").style.pointerEvents = "all";
+    });
+})
+.catch(error => console.error('Error fetching data:', error));
