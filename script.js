094e234b3f73ead3a43cf7294eb339bd6a912234
Nathan Hadley <nathanhadley.pigeon@gmail.com>
2024-07-01 10:45:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const apiKey = "eyJ4NXQiOiJOak16WWpreVlUZGlZVGM0TUdSalpEaGtaV1psWWpjME5UTXhORFV4TlRZM1ptRTRZV1JrWWc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9";
-const apiURL = `https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=50.371389&longitude=-4.142222`;
+const apiURL = "https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=50.371389&longitude=-4.142222";
 
 // This doesn't work, returns invalid credentials
-
 fetch(apiURL, {headers: {apikey: apiKey}})
 .then(response => response.json())
 .then(data => {
